1d8b9b448c30fdacebac01b53a3761a8120f3b39
gio939425 <gio939425@gmail.com>
2026-08-31 17:35:19 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-
+function estimate() {
+ 
+let sqft =
+document.getElementById("sqft").value;
+ 
+let total = sqft * 180;
+ 
+document.getElementById("result").innerHTML =
+"Estimated Cost: $" +
+total.toLocaleString();
+ 
+}
